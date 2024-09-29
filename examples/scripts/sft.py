13477e9bf21293b3fa4ed7ce83161681f9604137
someguy1597 <137055086+someguy1597@users.noreply.github.com>
2024-09-29 10:39:08 +0330

    Update sft.py
    commented out device_map="auto"

--- examples/scripts/sft.py
+++ examples/scripts/sft.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
         attn_implementation=model_config.attn_implementation,
         torch_dtype=model_config.torch_dtype,
         use_cache=False if training_args.gradient_checkpointing else True,
-        device_map="auto",
+        #device_map="auto",
         quantization_config=quantization_config,
     )
     #training_args.model_init_kwargs = model_kwargs
